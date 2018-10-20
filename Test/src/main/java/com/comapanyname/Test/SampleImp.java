0b65039b0365b78d0e081d2408be1446601bc3fd
sys <sys@DESKTOP-GJ0UH62>
2018-10-20 17:57:18 +0530

    SampleInterface definition and implementation

---
+++ Test/src/main/java/com/comapanyname/Test/SampleImp.java
@@ -0,0 +1,19 @@
+package com.comapanyname.Test;
+
+public class SampleImp implements Sample {
+	public void stringTest()
+	 {
+		String str1="hello!! String primitive";
+		System.out.println("String datatype:"+str1);
+	 }
+	public void intTest()
+	{
+		int num1=10;
+		System.out.println("integer datatype:"+num1);
+	}
+	public void booleanTest()
+	{
+		boolean eq=true;
+		System.out.println("boolean datatype:"+eq);
+	}
+}
