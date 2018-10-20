694dc7d8990730f567a3eb4bd7bdd83a06375aa9
sys <sys@DESKTOP-GJ0UH62>
2018-10-20 17:49:48 +0530

    create a new class

---
+++ Test/src/main/java/com/comapanyname/Test/Test.java
@@ -0,0 +1,11 @@
+package com.comapanyname.Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("testing git commands");
+	}
+
+}
